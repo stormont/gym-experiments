8c12f4447a332a8394d7877518279bcc2d682f91
Devan Stormont <stormont@gmailcom>
2018-08-06 17:12:09 -0500

    Make variables plural

--- helpers/model.py
+++ helpers/model.py
@@ -8,8 +8,8 @@ class ModelWrapper:
     def action_size(self):
         return self._model.layers[-1].output_shape[1]
 
-    def fit(self, state, prediction):
-        return self._model.fit(state, prediction, epochs=1, verbose=0, batch_size=self._fit_batch_size)
+    def fit(self, states, predictions):
+        return self._model.fit(states, predictions, epochs=1, verbose=0, batch_size=self._fit_batch_size)
 
     def predict(self, state):
         return self._model.predict(state)
